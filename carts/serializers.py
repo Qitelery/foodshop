8c76905c05f404bacac9897a23043dd395125f96
Qitelery <alexeydronov@gmail.com>
2021-05-10 22:43:42 +0300

    week2 total_price for POST carts/items/

--- carts/serializers.py
+++ carts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+from rest_framework.response import Response
 
 from carts.models import Cart, CartItem
+from items.models import Item
 
 
 class CartSerializer(serializers.ModelSerializer):
@@ -16,8 +18,21 @@ class CartItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CartItem
-        fields = ['id', 'item', 'quantity', 'total_price']
+        fields = ['id', 'item', 'quantity', 'price', 'total_price']
+        read_only_fields = ['price']
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+        new_item = CartItem()
+        new_item.item_id = self.validated_data['item'].id
+        new_item.cart_id = cart.id
+        new_item.quantity = self.validated_data['quantity']
+        new_item.price = self.validated_data['item'].price
+
+        new_item.save()
+
+        return new_item
 
     def get_total_price(self, obj):
-        print(obj)
-        return obj['item']['quantity'] * obj['item']['price']
+        return obj.quantity * obj.price
--- carts/views.py
+++ carts/views.py
@@ -14,19 +14,5 @@ class CartViewSet(ReadOnlyModelViewSet):
 
 
 class CartItemViewSet(ModelViewSet):
-
     serializer_class = CartItemSerializer
     permission_classes = [IsAuthenticated]
-
-    def create(self, request, *args, **kwargs):
-        cart, _ = Cart.objects.get_or_create(user=request.user)
-        cart_item = CartItemSerializer(data=request.data)
-        if cart_item.is_valid():
-            item = Item.objects.get(id=request.data.get('item'))
-            new_cart_item = CartItem.objects.create(
-                item_id=cart_item.data['item'],
-                cart_id=cart.id,
-                quantity=cart_item.data['quantity'],
-                price=item.price,
-            )
-            return Response(CartItemSerializer(new_cart_item).data)
